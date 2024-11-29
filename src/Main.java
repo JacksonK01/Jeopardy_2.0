71f3863309e2441445d2826acb7e7a46804b55c5
JacksonK <jackmkirchner@gmail.com>
2024-11-29 15:58:06 -0600

    FIX HOPEFULLY

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
 import screen.Screen;
 
 public class Main {
-
     public static void main(String[] args) {
         Screen frame = new Screen();
         frame.setVisible(true);
 
         Thread thread = new Thread(frame);
         thread.start();
-
-//        JeopardyBoard jeopardyBoard1 = new JeopardyBoard();
-//
-//        String jeoJson = JsonHandler.readJsonFromFile("default");
-//
-//        System.out.println(jeoJson);
-//
-//        JsonHandler.exportJsonToFile(JsonHandler.serializeBoard(jeopardyBoard1), "default");
     }
 }
